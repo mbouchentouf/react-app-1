6d4d4afe73e4aaf78f4bd1721e13533926d8aa26
Massimo Bouchentouf <mbouchentouf@cnm.edu>
2021-09-04 03:03:51 -0600

    added: Content.js, Header.js, Sidebar.js Components

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,20 +1,24 @@
 import React from 'react';
+import {FaPizzaSlice} from "react-icons/fa";
 
 export const Header = () => {
     const x = 1;
 
-    return <header className="header" data-tested="header">
-        <nav>
+    return (
+        <header className="header" data-tested="header">
+           <nav>
             <div className="logo">
-                <img src="/images/logo.png" alt="react-app" />
+                <img src="/images/logo.png" alt="react-app-1" />
             </div>
             <div className="settings">
                 <ul>
                     <li>+</li>
-                    <li>Pizza Slice!</li>
+                    <li>
+                        <FaPizzaSlice/>
+                    </li>
                 </ul>
             </div>
         </nav>
-    </header>;
-
+    </header>
+);
 };
