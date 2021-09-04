ca78ea552ba78a6985ccb043bad89faa7d56c8d6
Massimo Bouchentouf <mbouchentouf@cnm.edu>
2021-09-04 05:00:20 -0600

    created firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+
+
+const firebaseConfig = firebase.initializeApp({
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectID: '',
+    storageBucket: '',
+    messageSenderId: '',
+    apiID: '',
+});
+
+export {firebaseConfig as firebase};
