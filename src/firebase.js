3914abffcc9141643007b19a6b58c5c9bbdcec76
Massimo Bouchentouf <mbouchentouf@cnm.edu>
2021-09-06 02:53:25 -0600

    added and generated firebase strings: apiKey, authDomain, databaseURL, projectId, storageBucket, messagingSenderId, appId

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 
 
 const firebaseConfig = firebase.initializeApp({
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectID: '',
-    storageBucket: '',
-    messageSenderId: '',
-    apiID: '',
+    apiKey: "AIzaSyBVXzbHLK-IVp0ecwA8ajjDbCsranaSo0E",
+    authDomain: "todoist-app-1571d.firebaseapp.com",
+    databaseURL: "https://todoist-app-1571d-default-rtdb.firebaseio.com",
+    projectId: "todoist-app-1571d",
+    storageBucket: "todoist-app-1571d.appspot.com",
+    messagingSenderId: "438332599202",
+    appId: "1:438332599202:web:f0b9e221c2f7245d1c20fb"
 });
 
 export {firebaseConfig as firebase};
